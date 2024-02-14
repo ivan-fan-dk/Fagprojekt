477759b41c455eea82e916f20fc5ee29bf73f9e9
HarrisNH <114136788+HarrisNH@users.noreply.github.com>
2024-02-14 10:23:32 +0000

    new data func

--- opgaver/functions.py
+++ opgaver/functions.py
@@ -13,44 +13,39 @@ def data1D(f, lower_bound, upper_bound,N):
     y = f(X)
     print(y)
     return torch.tensor(X, dtype=torch.float32).view(-1, 1), torch.tensor(y, dtype=torch.float32).view(-1, 1)
-def data2D(f, lower_bound, upper_bound,N):
-    """
-    input: function's expression f, and input data X
-    output: return labels y
-    """
-    X = np.linspace(lower_bound, upper_bound, N)
-    print(X)
-    y = f(X)
-    print(y)
-    return torch.tensor(X, dtype=torch.float32).view(-1, 1), torch.tensor(y, dtype=torch.float32).view(-1, 1)
 
-
-def data2D(f, lower_bound, upper_bound,N,inputdims):
+def data(f, lower_bound, upper_bound,N,inputdims):
     """
     input: function's expression f, and input data X
     output: return labels y
     """
 
     X = np.linspace(lower_bound, upper_bound, N)
-    if inputdims > 1:
-        
-        for i in range(inputdims-1):
-            Y = np.linspace(lower_bound, upper_bound, N)
-    
+    Y = []
+    for i in range(inputdims-1):
+        Y.append(np.linspace(lower_bound, upper_bound, N))
+    print(len(Y))
     print(X)
-    y = f(X)
-    print(y)
-    return torch.tensor(X, dtype=torch.float32).view(-1, 1), torch.tensor(y, dtype=torch.float32).view(-1, 1)
-def data2D(f, lower_bound, upper_bound,N):
-    """
-    input: function's expression f, and input data X
-    output: return labels y
-    """
-    X = np.linspace(lower_bound, upper_bound, N)
-    print(X)
-    y = f(X)
-    print(y)
-    return torch.tensor(X, dtype=torch.float32).view(-1, 1), torch.tensor(y, dtype=torch.float32).view(-1, 1)
+    if inputdims == 1:
+        z = f(X)
+        return torch.tensor(X, dtype=torch.float32).view(-1, 1), torch.tensor(z, dtype=torch.float32).view(-1, 1)
+    elif inputdims == 2:
+        z = f(X,Y[0])
+        return torch.tensor(X, dtype=torch.float32).view(-1, 1),torch.tensor(Y[0], dtype=torch.float32).view(-1, 1), torch.tensor(z, dtype=torch.float32).view(-1, 1)
+    elif inputdims == 3: 
+        z = f(X,Y[0],Y[1])
+        return torch.tensor(X, dtype=torch.float32).view(-1, 1),torch.tensor(Y[0], dtype=torch.float32).view(-1, 1),torch.tensor(Y[1], dtype=torch.float32).view(-1, 1), torch.tensor(z, dtype=torch.float32).view(-1, 1)
+    elif inputdims == 4: 
+        z = f(X,Y[0],Y[1],Y[2])
+        return torch.tensor(X, dtype=torch.float32).view(-1, 1),torch.tensor(Y[0], dtype=torch.float32).view(-1, 1),torch.tensor(Y[1], dtype=torch.float32).view(-1, 1),torch.tensor(Y[2], dtype=torch.float32).view(-1, 1),torch.tensor(z, dtype=torch.float32).view(-1, 1)
+    elif inputdims == 5: 
+        z = f(X,Y[0],Y[1],Y[2],Y[3])
+        return torch.tensor(X, dtype=torch.float32).view(-1, 1),torch.tensor(Y[0], dtype=torch.float32).view(-1, 1),torch.tensor(Y[1], dtype=torch.float32).view(-1, 1),torch.tensor(Y[2], dtype=torch.float32).view(-1, 1),torch.tensor(Y[3], dtype=torch.float32).view(-1, 1),torch.tensor(z, dtype=torch.float32).view(-1, 1)
+    else: return print("Dim not supported")
+
+ 
+X0,X1,X2,X3,z = data(lambda x0,x1,x2,x3: np.sin(x0)*x1+np.cos(x2)+x3,0,10,100,4)
+print(z)
 
 # X = torch.tensor(X, dtype=torch.float32).view(-1, 1)
 # y = torch.tensor(y, dtype=torch.float32).view(-1, 1)
