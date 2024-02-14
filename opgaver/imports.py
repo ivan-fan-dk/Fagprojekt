2d60080f3a0ddb119b59488bf5074ffb5ba59476
HarrisNH <114136788+HarrisNH@users.noreply.github.com>
2024-02-14 09:58:50 +0000

    opgaver

--- opgaver/imports.py
+++ opgaver/imports.py
@@ -1,5 +1,2 @@
-import numpy as np
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import matplotlib.pyplot as plt
+
+
--- opgaver/opgave_a.py
+++ opgaver/opgave_a.py
@@ -1,3 +1,51 @@
-from imports import *
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import matplotlib.pyplot as plt
+import functions as fc
+X,y = fc.data(lambda x,: np.sin(x)*x+np.cos(x),0,10,100)
 
-X,y = fc.f(sin(x)*x+cos(x))
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+        self.layer1 = nn.Linear(1, 10, bias=True)
+        self.layer2 = nn.Linear(10, 50, bias=True)
+        self.layer3 = nn.Linear(50, 30, bias=True)
+        self.layer4 = nn.Linear(30, 10, bias=True)
+        self.layer5 = nn.Linear(10, 1, bias=True)
+
+
+    def forward(self, x):
+        x = torch.relu(self.layer1(x))
+        x = torch.relu(self.layer2(x))
+        x = torch.relu(self.layer3(x))
+        x = torch.relu(self.layer4(x))
+        x = self.layer5(x)
+        return x
+
+# Create an instance of the model, define loss and optimizer
+model = NeuralNetwork()
+criterion = nn.MSELoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+# Training the model
+num_epochs = 20000
+for epoch in range(num_epochs):
+    # Forward pass
+    outputs = model(X)
+    loss = criterion(outputs, y)
+
+    # Backward pass and optimization
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    if (epoch + 1) % 10 == 0:
+        print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
+
+# Evaluate the model
+with torch.no_grad():
+    predictions = model(X)
+    loss = criterion(predictions, y)
+    print(f"Mean Squared Error on test data: {loss.item():.4f}")
