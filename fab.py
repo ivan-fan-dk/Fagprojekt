78a29c1a53ab58fe98eae81ee136badc8f59ebc3
FabianSWuertz <159913116+FabianSWuertz@users.noreply.github.com>
2024-02-14 07:55:10 +0000

    aogbtrorjeg

--- fab.py
+++ fab.py
@@ -1,11 +1,13 @@
+# Kræver 
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import matplotlib.pyplot as plt
+
+#import main
+
 # Enten 'cubic', 'sine', 'abosulute' 'exp_sin'
-
-# Kræver import torch
-#import torch.nn as nn
-#import torch.optim as optim
-#import numpy as np
-#import matplotlib.pyplot as plt
-
 def neural_approx(choice):
 
     functions = {
